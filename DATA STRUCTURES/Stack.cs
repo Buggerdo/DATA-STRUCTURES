7cc41e313af2ec6ad388a8463d195b52d8a9ec8a
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-07-26 10:30:22 -0400

    Did some stuff

--- DATA STRUCTURES/Stack.cs
+++ DATA STRUCTURES/Stack.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DATA_STRUCTURES
+﻿namespace DATA_STRUCTURES
 {
     internal class Stack
     {
+        public Stack()
+        {
+            Console.Write("Please enter a word you would like to reverse: ");           
+            Console.WriteLine(Reverse(Validator.UserInput()));
+        }
+
+        private static string Reverse(string str)
+        {
+            string[] strArray = str.Split(' ');
+            Stack<char> stack = new();
+            string reversedString = string.Empty;
+            foreach(var item in strArray)
+            {
+                item.ToCharArray().ToList().ForEach(stack.Push);
+                
+                for(int i = 0; i < item.Length; i++)
+                {
+                    reversedString += stack.Pop();
+                }
+                reversedString +=  " ";
+            }
+            return reversedString.Trim();
+        }
     }
 }
--- DATA STRUCTURES/Validator.cs
+++ DATA STRUCTURES/Validator.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text.RegularExpressions;
 
 namespace DATA_STRUCTURES
 {
     internal class Validator
     {
+        public static string UserInput()
+        {
+            var regexItem = new Regex("^[a-z A-Z]*$");
+
+            string input = Console.ReadLine();
+            if(regexItem.IsMatch(input))
+            {
+                return input;
+            }
+            return "ehT gnirts uoy deretne saw dilavni"; // Because I can
+        }
     }
 }
